c17122217ac61e8aa5bcc850cf9cad2a631518f2
Nikita Konopelko <nik.sativa@gmail.com>
2022-04-18 09:32:27 +0300

    new formating rules

--- TestHelpers/Queue+TestHelper.swift
+++ TestHelpers/Queue+TestHelper.swift
@@ -3,11 +3,8 @@ import NSpry
 
 import NQueue
 
-extension Queue: SpryEquatable {
-}
+extension Queue: SpryEquatable {}
 
-extension Queue.Attributes: SpryEquatable {
-}
+extension Queue.Attributes: SpryEquatable {}
 
-extension Queue.Flags: SpryEquatable {
-}
+extension Queue.Flags: SpryEquatable {}
