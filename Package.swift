7c6204415d409a0ca94cc683807a0cf0af924879
Nikita Konopelko <nik.sativa@gmail.com>
2023-02-14 13:25:27 +0200

    1. swift-tools-version:5.6 2. min ios 13 and mac 10.15 versions 3. Quick/Nimble update

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 
 import PackageDescription
 
 // swiftformat:disable all
 let package = Package(
     name: "NQueue",
-    platforms: [.iOS(.v10), .macOS(.v10_12)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "NQueue", targets: ["NQueue"]),
         .library(name: "NQueueTestHelpers", targets: ["NQueueTestHelpers"])
     ],
     dependencies: [
         .package(url: "https://github.com/NikSativa/NSpry.git", .upToNextMajor(from: "1.2.9")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "10.0.0"))
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "6.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "11.2.1"))
     ],
     targets: [
         .target(name: "NQueue",
