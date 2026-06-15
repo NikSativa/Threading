ddee847dbf1228bac86c8e538ce72cd69b5c82c0
Mykyta Konopelko <nik.sativa@gmail.com>
2026-06-15 21:49:39 +0300

    lower iOS minimum to 15

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Threading",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v15),
         .macOS(.v14),
-        .macCatalyst(.v16),
+        .macCatalyst(.v15),
         .tvOS(.v16),
         .watchOS(.v9),
         .visionOS(.v1)
