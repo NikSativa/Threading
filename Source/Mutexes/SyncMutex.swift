e5f14bf3a311619513d9e9dc3ffd7cefef316d4b
Mykyta Konopelko <nik.sativa@gmail.com>
2026-09-07 11:56:25 +0300

    Fix misdirected Sendable conformance on SyncMutex

--- Source/Mutexes/SyncMutex.swift
+++ Source/Mutexes/SyncMutex.swift
@@ -80,5 +80,5 @@ extension SyncMutex: Mutexing {
 }
 
 @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
-extension QueueBarrier: @unchecked Sendable {}
+extension SyncMutex: Sendable {}
 #endif
